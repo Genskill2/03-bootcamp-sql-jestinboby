e7ce3fdba0544ae7c15690e7b24691e11de70c9a
jestinboby <jestinboby@gmail.com>
2021-06-17 15:19:37 +0530

    SQL

--- query3.sql
+++ query3.sql
@@ -1,1 +1,1 @@
-select books.title from books inner join books_subjects on books_subjects.subject.book=books.id where books_subjects.subject='3' or books_subjects.subject='8';
+select books.title from books inner join books_subjects on books_subjects.book=books.id where books_subjects.subject='3' or books_subjects.subject='8';
